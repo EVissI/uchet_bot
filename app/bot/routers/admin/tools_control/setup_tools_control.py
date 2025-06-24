9505ef298b0afc65e1b62583eb2570ee72ee4b9b
viss <egor-berezin@bk.ru>
2025-06-25 03:34:58 +1000

    commit

--- app/bot/routers/admin/tools_control/setup_tools_control.py
+++ app/bot/routers/admin/tools_control/setup_tools_control.py
@@ -22,7 +22,6 @@ setup_tools_control_router.include_routers(
 )
 
 @setup_tools_control_router.message(F.text.in_(get_all_texts('instrument_control_btn')),
-                                    StateFilter(None),
                                     UserInfo())
 async def create_tool_control_kbd(message:Message, state:FSMContext, user_info:User):
     await message.delete()
