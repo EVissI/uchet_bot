c4d2052469c59728e23c748ca7cbb626a9dd704e
viss <egor-berezin@bk.ru>
2025-06-30 23:49:51 +1000

    commit

--- app/bot/routers/general_routers/object_control/create_object_router.py
+++ app/bot/routers/general_routers/object_control/create_object_router.py
@@ -102,7 +102,7 @@ async def process_object_description(message: Message, state: FSMContext, user_i
     )
     await create_object_router.save_message(state, bot_message.message_id)  
 
-@create_object_router.message(F.document, StateFilter(CreateObjectStates.waiting_documents), UserInfo())
+@create_object_router.message(F.document.mime_type == "application/pdf", StateFilter(CreateObjectStates.waiting_documents), UserInfo())
 async def process_object_documents(message: Message, state: FSMContext, user_info: User):
     await create_object_router.save_message(state, message.message_id)
     data = await state.get_data()
@@ -136,7 +136,7 @@ async def finish_document_received(message: Message, state: FSMContext, user_inf
     await send_next_document(message, state, user_info)
 
 
-@create_object_router.message(~F.document, StateFilter(CreateObjectStates.waiting_documents), UserInfo())
+@create_object_router.message(F.document.mime_type != "application/pdf", StateFilter(CreateObjectStates.waiting_documents), UserInfo())
 async def unprocess_object_documents(message: Message, state: FSMContext, user_info: User):
     await create_object_router.save_message(state, message.message_id)
     bot_message = await message.answer(
